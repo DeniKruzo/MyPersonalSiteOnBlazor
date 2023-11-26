4598434755a341d4810b0a96d2cef107bd82b816
DeniKruzo <kruzodanil@mail.ru>
2023-11-26 18:00:48 +0500

    Add project files.

---
+++ BlazorServTest/Data/Models/HardSkills.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServTest.Data.Models
+{
+    public class HardSkills
+    {
+        public string Name { get; set; } = "C#";
+
+        public DateTime Since { get; set; }
+
+        public string CurrentLevel { get; set; } = "Beginner";
+    }
+}
